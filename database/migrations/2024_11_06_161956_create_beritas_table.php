1020a2619148f5b331b447b87cd61e48c9dbb7f0
TheoWongkar <theoterra19@gmail.com>
2024-11-07 00:29:14 +0800

    membuat model dan mcf berita

---
+++ database/migrations/2024_11_06_161956_create_beritas_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('kategories', function (Blueprint $table) {
+            $table->id();
+            $table->string('category');
+            $table->timestamps();
+        });
+
+        Schema::create('beritas', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('category_id')->constrained('kategories')->onDelete('cascade');
+            $table->string('title');
+            $table->string('slug');
+            $table->string('image');
+            $table->text('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('kategories');
+        Schema::dropIfExists('beritas');
+    }
+};
