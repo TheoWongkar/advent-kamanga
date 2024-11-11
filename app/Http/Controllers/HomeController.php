65a898d906f776c0cddc83fe69d50ae3c183f341
TheoWongkar <theoterra19@gmail.com>
2024-11-11 23:42:36 +0800

    menetapkan layout home guest

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Models\Worship;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $posts = Post::with('user', 'category')
+            ->orderBy('created_at', 'DESC');
+
+        $worship = Worship::orderBy('date', 'desc')->first();
+
+        return view('home', compact('worship', 'posts'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -29,36 +29,52 @@
 
         <!-- Event Section with Image -->
         <section class="flex items-center bg-[#375E97] text-white rounded-lg overflow-hidden shadow-lg">
-            <!-- Kontainer Gambar di Sisi Kiri tanpa Padding -->
             <div class="w-1/2">
                 <img src="https://catholicstewardship.com/wp-content/uploads/2021/12/christmas-advent-wreath-for-website_orig-e1670267637705.jpg"
                     alt="Gambar Deskripsi" class="w-full h-full object-cover">
             </div>
-
-            <!-- Kontainer Tulisan di Sisi Kanan -->
             <div class="w-1/2 pl-6">
-                <h2 class="font-bold text-2xl mb-4">JADWAL IBADAH TERBARU</h2>
-                <h2 class="font-semibold text-xl mb-1">Ibadah Hari Minggu</h2>
-                <p class="text-base mb-2">10/12/2024</p>
-                <p class="mb-1">Khotbah :</p>
-                <p class="mb-6">Singers :</p>
-                <button class="bg-[#FB6542] hover:bg-orange-600 text-white py-2 px-4 rounded-xl">
-                    Lihat Jadwal Lainnya...
-                </button>
+                @if ($worship)
+                    <h2 class="font-bold text-2xl mb-4">JADWAL IBADAH TERBARU</h2>
+                    <h2 class="font-semibold text-xl mb-1">{{ $worship->title }}</h2>
+                    <p class="text-base mb-2">{{ \Carbon\Carbon::parse($worship->date)->format('M Y') }}</p>
+                    <p class="mb-1">Khotbah : {{ $worship->preacher }}</p>
+                    <p class="mb-6">Singers : {{ $worship->singer }}</p>
+                    <a href="/ibadah" class="bg-[#FB6542] hover:bg-orange-600 text-white py-2 px-4 rounded-xl">
+                        Lihat Jadwal Lainnya...
+                    </a>
+                @else
+                    <h2 class="font-bold text-2xl mb-4">JADWAL IBADAH TERBARU</h2>
+                    <h2 class="font-semibold text-xl mb-1">Belum ada jadwal ibadah</h2>
+                    <p class="mb-1">Khotbah : belum ada pengkhotbah</p>
+                    <p class="mb-6">Singers : belum ada penyanyi</p>
+                @endif
             </div>
         </section>
 
         <!-- Cards Section -->
-        <section class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-            <div class="bg-white rounded-lg shadow-md overflow-hidden">
-                <img src="https://via.placeholder.com/300x200" alt="Image" class="w-full h-40 object-cover">
-                <div class="p-4">
-                    <p class="text-xs sm:text-sm text-gray-600">Date
-                    </p>
-                    <h4 class="text-base sm:text-lg font-bold text-gray-800">Lorem Ipsum Dolor Amet</h4>
+        @if ($posts->count())
+            @foreach ($posts as $post)
+                <section class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                    <div class="bg-white rounded-lg shadow-md overflow-hidden">
+                        <img src="https://via.placeholder.com/300x200" alt="Image" class="w-full h-40 object-cover">
+                        <div class="p-4">
+                            <p class="text-xs sm:text-sm text-gray-600">Date
+                            </p>
+                            <h4 class="text-base sm:text-lg font-bold text-gray-800">Lorem Ipsum Dolor Amet</h4>
+                        </div>
+                    </div>
+                </section>
+            @endforeach
+        @else
+            <section class="container mx-auto">
+                <div class="bg-white rounded-lg shadow-md overflow-hidden">
+                    <div class="p-4">
+                        <h4 class="text-base sm:text-lg font-bold text-center">Belum ada berita.</h4>
+                    </div>
                 </div>
-            </div>
-        </section>
+            </section>
+        @endif
     </div>
 
 </x-guest-layout>
